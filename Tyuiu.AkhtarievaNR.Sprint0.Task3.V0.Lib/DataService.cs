74ee4f9e66b4bc0a134c5480c97215a9ad892b9b
nailaahtarieva <nuscha2006@mail.ru>
2024-10-01 19:39:45 +0500

    Task3

---
+++ Tyuiu.AkhtarievaNR.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.AkhtarievaNR.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        // Реализация метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
